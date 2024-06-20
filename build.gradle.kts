6bbe1a6bfafc74322380944389d3eca0f9f0ea52
jaguililla <juanjoaguililla@gmail.com>
2024-06-20 19:44:44 +0200

    Update dependencies

--- build.gradle.kts
+++ build.gradle.kts
@@ -8,7 +8,7 @@ plugins {
 }
 
 val hexagonVersion = "3.5.3"
-val flywayVersion = "10.13.0"
+val flywayVersion = "10.14.0"
 val postgresqlVersion = "42.7.3"
 val kafkaVersion = "3.7.0"
 val gradleScripts = "https://raw.githubusercontent.com/hexagonkt/hexagon/$hexagonVersion/gradle"
@@ -30,9 +30,9 @@ description="Service's description"
 dependencies {
     "implementation"("com.hexagonkt:http_server_netty:$hexagonVersion")
     "implementation"("com.hexagonkt:serialization_jackson_json:$hexagonVersion")
-    "implementation"("org.flywaydb:flyway-core:$flywayVersion")
-    "implementation"("org.postgresql:postgresql:$postgresqlVersion")
-    "implementation"("org.apache.kafka:kafka-clients:$kafkaVersion")
+//    "implementation"("org.flywaydb:flyway-core:$flywayVersion")
+//    "implementation"("org.postgresql:postgresql:$postgresqlVersion")
+//    "implementation"("org.apache.kafka:kafka-clients:$kafkaVersion")
 
     "testImplementation"("com.hexagonkt:http_client_jetty:$hexagonVersion")
 }
